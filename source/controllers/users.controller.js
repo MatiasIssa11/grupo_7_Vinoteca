388943aeed0064dcf60c2e994de364eb189143c6
AgustinCollados <collados_agustin@hotmail.com>
2022-07-02 13:32:45 -0300

    Correccion controllador users

--- source/controllers/users.controller.js
+++ source/controllers/users.controller.js
@@ -3,9 +3,9 @@ module.exports = {
     return res.render("./users/register", {
       title: "Cava Wines-Registro",
       styles: [
-        "/users/login-mobile",
-        "/users/login-tablet",
-        "/users/login-desktop",
+        "/users/register-mobile",
+        "/users/register-tablet",
+        "/users/register-desktop",
       ],
     });
   },
@@ -14,9 +14,9 @@ module.exports = {
     return res.render("./users/login", {
       title: "Cava Wines-Acceso",
       styles: [
-        "/users/register-mobile",
-        "/users/register-tablet",
-        "/users/register-desktop",
+        "/users/login-mobile",
+        "/users/login-tablet",
+        "/users/login-desktop",
       ],
     });
   },
