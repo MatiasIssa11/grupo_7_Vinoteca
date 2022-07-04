172b903efe0c9014fe3fa8b2da1627285d1a6790
alonsomatias88 <alonso.matias@live.com.ar>
2022-07-04 14:46:13 -0300

    Mati A: Agregado de descripción y composición en formato array

--- source/modules/data.js
+++ source/modules/data.js
@@ -6,10 +6,16 @@ module.exports = [
     type: "Malbec Roble Organico",
     location: "Maipu Mendoza",
     image: "images/malbec-roble.jpg",
-    composition:
-      "Alcohol. 13,30% \n Acidez total. 5,50 g/l \n Azúcar residual. 1,90 g/l",
-    description:
-      "Vista: Rojo oscuro con reflejos bordó.\nNariz: De gran complejidad aromática, se perciben notas a fruta madura que recuerdan a las ciruelas, al agitar la copa aparecen notas a vainilla, caramelo y chocolate.\nBoca: Vino aterciopelado, con taninos estructurados, de baja acidez, con un retrogusto agradable.",
+    composition: [
+      "Alcohol. 13,30%",
+      "Acidez total. 5,50 g/l",
+      "Azúcar residual. 1,90 g/l",
+    ],
+    description: [
+      "Vista: Rojo oscuro con reflejos bordó.",
+      "Nariz: De gran complejidad aromática, se perciben notas a fruta madura que recuerdan a las ciruelas, al agitar la copa aparecen notas a vainilla, caramelo y chocolate.",
+      "Boca: Vino aterciopelado, con taninos estructurados, de baja acidez, con un retrogusto agradable.",
+    ],
   },
   {
     id: 2,
@@ -18,10 +24,16 @@ module.exports = [
     type: "Cabernet Sauvignon",
     location: "Maipu Mendoza",
     image: "images/cabernet-Sauvignon.jpg",
-    composition:
-      "Alcohol. 13,30%\nAcidez total. 5,30 g/l\nAzúcar residual. 1,80 g/l",
-    description:
-      "Vista: Rojo intenso con reflejos violáceos.\nNariz: Vino joven, fresco, con notas a pimiento verde, propias de la variedad, con dejos a cerezas y ciruelas maduras.\nBoca: De entrada agradable, con taninos amables, de estructura media.",
+    composition: [
+      "Alcohol. 13,30%",
+      "Acidez total. 5,30 g/l",
+      "Azúcar residual. 1,80 g/l",
+    ],
+    description: [
+      "Vista: Rojo intenso con reflejos violáceos.",
+      "Nariz: Vino joven, fresco, con notas a pimiento verde, propias de la variedad, con dejos a cerezas y ciruelas maduras.",
+      "Boca: De entrada agradable, con taninos amables, de estructura media.",
+    ],
   },
   {
     id: 3,
@@ -30,10 +42,16 @@ module.exports = [
     type: "Malbec",
     location: "Maipu Mendoza",
     image: "images/malbec.jpg",
-    composition:
-      "Alcohol. 13,30%\nAcidez total. 5,40 g/l\nAzúcar residual. 1,90 g/l",
-    description:
-      "Vista: Rojo cereza brillante.\nNariz: Se perciben notas a frutas rojas maduras, que recuerdan a las ciruelas, guindas y moras, con toques sutiles a violetas y regaliz.\nBoca: Vino ligero, fácil de beber, de estructura media y alta frescura, con un agradable retrogusto.",
+    composition: [
+      "Alcohol. 13,30%",
+      "Acidez total. 5,40 g/l",
+      "Azúcar residual. 1,90 g/l",
+    ],
+    description: [
+      "Vista: Rojo cereza brillante.",
+      "Nariz: Se perciben notas a frutas rojas maduras, que recuerdan a las ciruelas, guindas y moras, con toques sutiles a violetas y regaliz.",
+      "Boca: Vino ligero, fácil de beber, de estructura media y alta frescura, con un agradable retrogusto.",
+    ],
   },
   {
     id: 4,
@@ -42,9 +60,17 @@ module.exports = [
     type: "Malbec sin Sulfito",
     location: "Maipu Mendoza",
     image: "images/malbec-sin-sulfito.jpg",
-    composition:
-      "Alcohol. 14%\nAcidez total. 5,3 g/l\nAzúcar residual. 1,80 g/l\nSO2 libre: 5,2 mg/l\nSO2 total: 14 mg/l",
-    description:
-      "Vista: Rojo rubí con reflejos violetas.\nNariz: Vino fresco, joven, se perciben aromas a fruta fresca que recuerdan a las frutillas, cerezas, ciruelas, con un dejo sutil a rosas.\nBoca: Entrada ligera, levemente ácido, evoluciona con el paso del tiempo, en donde se acentúa lo percibido en nariz.",
+    composition: [
+      "Alcohol. 14%",
+      "Acidez total. 5,3 g/l",
+      "Azúcar residual. 1,80 g/l",
+      "SO2 libre: 5,2 mg/l",
+      "SO2 total: 14 mg/l",
+    ],
+    description: [
+      "Vista: Rojo rubí con reflejos violetas.",
+      "Nariz: Vino fresco, joven, se perciben aromas a fruta fresca que recuerdan a las frutillas, cerezas, ciruelas, con un dejo sutil a rosas.",
+      "Boca: Entrada ligera, levemente ácido, evoluciona con el paso del tiempo, en donde se acentúa lo percibido en nariz.",
+    ],
   },
 ];
