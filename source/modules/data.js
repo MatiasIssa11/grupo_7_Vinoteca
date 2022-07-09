2002a3e5061e68ac0e56b051003ce2c8d2f6be0d
AgustinCollados <collados_agustin@hotmail.com>
2022-07-09 18:36:44 -0300

    Modificacion productos y caracteristicas - no anda aun

--- source/modules/data.js
+++ source/modules/data.js
@@ -4,62 +4,104 @@ module.exports = [
     price: 6087,
     name: "Terravita Malbec Roble Organico",
     type: "Malbec Roble Organico",
-    location: "Maipu Mendoza",
     image: "images/malbec-roble.jpg",
+
+    alcohol: "13,30%",
+    acidez: "5,50 g/l",
+    azucar: "1,90 g/l",
+      
+    /*
     composition: [
       "Alcohol. 13,30%",
       "Acidez total. 5,50 g/l",
       "Azúcar residual. 1,90 g/l",
     ],
+    */
+    
+    vista: "Rojo oscuro con reflejos bordó.",
+    nariz: "De gran complejidad aromática, se perciben notas a fruta madura que recuerdan a las ciruelas, al agitar la copa aparecen notas a vainilla, caramelo y chocolate.",
+    boca: "Vino aterciopelado, con taninos estructurados, de baja acidez, con un retrogusto agradable.",
+
+    /*  
     description: [
       "Vista: Rojo oscuro con reflejos bordó.",
       "Nariz: De gran complejidad aromática, se perciben notas a fruta madura que recuerdan a las ciruelas, al agitar la copa aparecen notas a vainilla, caramelo y chocolate.",
       "Boca: Vino aterciopelado, con taninos estructurados, de baja acidez, con un retrogusto agradable.",
     ],
+    */
   },
   {
     id: 2,
     price: 5072,
     name: "Terravita Cabernet Sauvignon",
     type: "Cabernet Sauvignon",
-    location: "Maipu Mendoza",
     image: "images/cabernet-Sauvignon.jpg",
+    
+    alcohol: "13,30%",
+    acidez: "5,30 g/l",
+    azucar: "1,80 g/l",
+      
+    /*
     composition: [
       "Alcohol. 13,30%",
       "Acidez total. 5,30 g/l",
       "Azúcar residual. 1,80 g/l",
-    ],
-    description: [
+    ], 
+    */
+    
+    vista: "Rojo intenso con reflejos violáceos.",
+    nariz: "Vino joven, fresco, con notas a pimiento verde, propias de la variedad, con dejos a cerezas y ciruelas maduras.",
+    boca: "De entrada agradable, con taninos amables, de estructura media.",
+
+   /* description: [
       "Vista: Rojo intenso con reflejos violáceos.",
       "Nariz: Vino joven, fresco, con notas a pimiento verde, propias de la variedad, con dejos a cerezas y ciruelas maduras.",
       "Boca: De entrada agradable, con taninos amables, de estructura media.",
-    ],
+    ], */
   },
   {
     id: 3,
     price: 5072,
     name: "Terravita Malbec",
     type: "Malbec",
-    location: "Maipu Mendoza",
     image: "images/malbec.jpg",
+
+    alcohol: "13,30%",
+    acidez: "5,40 g/l",
+    azucar: "1,90 g/l",
+
+    /*
     composition: [
       "Alcohol. 13,30%",
       "Acidez total. 5,40 g/l",
       "Azúcar residual. 1,90 g/l",
     ],
+    */
+    
+    vista: "Rojo cereza brillante.",
+    nariz: "Se perciben notas a frutas rojas maduras, que recuerdan a las ciruelas, guindas y moras, con toques sutiles a violetas y regaliz.",
+    boca: "Vino ligero, fácil de beber, de estructura media y alta frescura, con un agradable retrogusto.",
+
+    /*
     description: [
       "Vista: Rojo cereza brillante.",
       "Nariz: Se perciben notas a frutas rojas maduras, que recuerdan a las ciruelas, guindas y moras, con toques sutiles a violetas y regaliz.",
       "Boca: Vino ligero, fácil de beber, de estructura media y alta frescura, con un agradable retrogusto.",
     ],
+    */
   },
   {
     id: 4,
     price: 5580,
     name: "Terravita Malbec sin Sulfito",
     type: "Malbec sin Sulfito",
-    location: "Maipu Mendoza",
     image: "images/malbec-sin-sulfito.jpg",
+
+    alcohol: "14%",
+    acidez: "5,30 g/l",
+    azucar: "1,80 g/l",
+
+    /*
     composition: [
       "Alcohol. 14%",
       "Acidez total. 5,3 g/l",
@@ -67,10 +109,18 @@ module.exports = [
       "SO2 libre: 5,2 mg/l",
       "SO2 total: 14 mg/l",
     ],
+    */
+
+    vista: "Rojo rubí con reflejos violetas.",
+    nariz: "Vino fresco, joven, se perciben aromas a fruta fresca que recuerdan a las frutillas, cerezas, ciruelas, con un dejo sutil a rosas.",
+    boca: "Entrada ligera, levemente ácido, evoluciona con el paso del tiempo, en donde se acentúa lo percibido en nariz.",
+
+    /*
     description: [
       "Vista: Rojo rubí con reflejos violetas.",
       "Nariz: Vino fresco, joven, se perciben aromas a fruta fresca que recuerdan a las frutillas, cerezas, ciruelas, con un dejo sutil a rosas.",
       "Boca: Entrada ligera, levemente ácido, evoluciona con el paso del tiempo, en donde se acentúa lo percibido en nariz.",
     ],
+    */
   },
 ];
