e3fe32c17fd8f23f7bcc0f42f02ac1aa2256907a
AgustinCollados <collados_agustin@hotmail.com>
2022-07-30 16:52:28 -0300

    Intento de implementacion de validaciones - no funciona aun

--- source/controllers/users.controller.js
+++ source/controllers/users.controller.js
@@ -1,4 +1,5 @@
 const { index, one, create, write } = require("../models/users.model");
+const { validationResult } = require('express-validator');
 
 module.exports = {
   register: (req, res) => {
@@ -12,13 +13,26 @@ module.exports = {
     });
   },
 
+  ////////////////////////////////////// Validacion - mensajes de error - No funca:
+  procesRegister: (req, res) => {
+      
+    const resultValidation = validationResult(req);
+
+    if (resultValidation.errors.lenght > 0) {
+      return res.render("./users/register", {
+        errors: resultValidation.mapped()
+      });
+    }
+  },
+/////////////////////////////////////
+
   save: (req, res) => {
-    req.body.avatar = req.files[0].filename;
-    let newUser = create(req.body);
-    let users = index();
-    users.push(newUser);
-    write(users);
-    return res.redirect("/users/login");
+    req.body.avatar = req.files[0].filename; // Levanta archivo del multer (el primero cargado)
+    let newUser = create(req.body); // Crea nuevo usuario
+    let users = index(); // Trae user.json como obj. literal
+    users.push(newUser); // Agrega nuevo usuario al final del objeto literal users
+    write(users); // Actualiza el user.json con el nuevo user
+    return res.redirect("/users/register");
   },
 
   login: (req, res) => {
--- source/routes/users.routes.js
+++ source/routes/users.routes.js
@@ -6,14 +6,27 @@ const {
   login,
   save,
   enter,
+  procesRegister,
 } = require("../controllers/users.controller");
+
+///////////////// Validacion incompleta - No funca:
+
+const validations = [
+  check('nombre').notEmpty().withMessage('Este campo es obligatorio'),
+  check('apellido').notEmpty().withMessage('Este campo es obligatorio'),
+  check('email').notEmpty().withMessage('Este campo es obligatorio'),
+  check('fechaNacimiento').notEmpty().withMessage('Este campo es obligatorio'),
+  check('password').notEmpty().withMessage('Este campo es obligatorio')
+];
+
+/////////////////
 
 const multer = require("multer");
 const storage = require("../modules/storage");
 const upload = multer({ storage: storage("users") });
 
 routes.get("/register", register);
-routes.post("/save", [upload.any()], save); //Guardado de datos en el registro
+routes.post("/save", [upload.any()], validations, save, procesRegister); //Guardado de datos en el registro
 
 routes.get("/login", login);
 routes.post("/enter", enter); //Actualmente solo redirige al index
