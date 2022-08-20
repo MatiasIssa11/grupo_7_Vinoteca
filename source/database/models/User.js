80b907c2bc3ec9dbf43414bb158b770e899ee6ff
AgustinCollados <collados_agustin@hotmail.com>
2022-08-20 19:35:28 -0300

    Cambio seeder index()

--- source/database/models/User.js
+++ source/database/models/User.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, DataTypes) => {
          allowNull: false,
          unique: true,
       },
-      fechaNaciemiento: {
+      fechaNacimiento: {
          type: Sequelize.DATE,
          allowNull: false,
       },
