8022ece8c9336f13ae9142d55b4ec90a4a58294c
AgustinCollados <collados_agustin@hotmail.com>
2022-09-10 22:26:11 -0300

    Avance validacion front create prod

---
+++ public/scripts/createProduct.js
@@ -0,0 +1,113 @@
+let form = document.forms[1]; //Por el nombre que tiene el id del formulario, fue la única forma de capturarlo y poder trabajar
+let inputs = form.elements;
+
+inputs.brand.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".create_msg-error-front.brand");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 2 })) {
+    msg = "La marca debe tener como mínimo de 2 caracteres";
+   }
+   
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.type.addEventListener("input", (e) => {
+   let fieldset = e.target.parentElement.parentElement;
+   let box = e.target.parentElement;
+   let value = e.target.value;
+   let feedback = fieldset.querySelector(".create_msg-error-front.type");
+   let msg = null;
+ 
+   if (!validator.isLength(value, { min: 4 })) {
+     msg = "La marca debe tener como mínimo de 4 caracteres";
+    }
+    
+   if (msg) {
+     box.classList.remove("valid");
+     box.classList.add("invalid");
+     feedback.innerText = msg;
+   } else {
+     box.classList.remove("invalid");
+     box.classList.add("valid");
+     feedback.innerText = "";
+   }
+   
+   inputs.otros.addEventListener("input", (e) => {
+      let fieldset = e.target.parentElement.parentElement;
+      let box = e.target.parentElement;
+      let value = e.target.value;
+      let feedback = fieldset.querySelector(".create_msg-error-front.otros");
+      let msg = null;
+    
+      if (!validator.isLength(value, { min: 20 })) {
+        msg = "La descripcion debe tener como mínimo de 20 caracteres";
+       }
+       
+      if (msg) {
+        box.classList.remove("valid");
+        box.classList.add("invalid");
+        feedback.innerText = msg;
+      } else {
+        box.classList.remove("invalid");
+        box.classList.add("valid");
+        feedback.innerText = "";
+      }
+   });
+ 
+   inputs.image.addEventListener("change", (e) => {
+      let fieldset = e.target.parentElement.parentElement;
+      let files = e.target.files;
+      let box = e.target.parentElement;
+      let feedback = fieldset.querySelector(".register_msg-error-front.image");
+      let msg = null;
+    
+      if (!files.length == 0) {
+        if (!validator.isMimeType(files[0].type)) {
+          msg = "La imagen no tiene un formato valido";
+        } else if (!["jpg", "svg", "png"].includes(files[0].type.split("/")[1])) {
+          msg = "La imagen no tiene una extension valida";
+        }
+      }
+    
+      if (msg) {
+        box.classList.remove("valid");
+        box.classList.add("invalid");
+        feedback.innerText = msg;
+      } else {
+        box.classList.remove("invalid");
+        box.classList.add("valid");
+        feedback.innerText = "";
+      }
+    });
+
+   form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      let isCorrect = false;
+
+      if (e.target.querySelectorAll(".login_linea-form.valid").length === 2) {
+         isCorrect = true;
+      }
+
+      if (isCorrect) {
+         e.target.submit();
+      } else {
+         window.alert(
+            "Algunos datos no son correctos, por favor verificar los errores en los campos"
+         );
+      };
+   });
+
+
+});
