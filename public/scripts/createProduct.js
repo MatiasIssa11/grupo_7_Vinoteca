124a9cac5a3bb0ce2cf3eb739674deefa6daffc6
AgustinCollados <collados_agustin@hotmail.com>
2022-09-15 18:47:11 -0300

    Agregado validaciones front a todos los campos de edit y create products

--- public/scripts/createProduct.js
+++ public/scripts/createProduct.js
@@ -44,6 +44,160 @@ inputs.type.addEventListener("input", (e) => {
     feedback.innerText = "";
   }
 
+  inputs.price.addEventListener("input", (e) => {
+    let fieldset = e.target.parentElement.parentElement;
+    let box = e.target.parentElement;
+    let value = e.target.value;
+    let feedback = fieldset.querySelector(".create_msg-error-front.price");
+    let msg = null;
+  
+    if (!validator.isLength(value, { min: 3 })) {
+      msg = "El precio no puede ser menor a 3 cifras.";
+    }
+  
+    if (msg) {
+      box.classList.remove("valid");
+      box.classList.add("invalid");
+      feedback.innerText = msg;
+    } else {
+      box.classList.remove("invalid");
+      box.classList.add("valid");
+      feedback.innerText = "";
+    }
+  });
+  
+  inputs.alcohol.addEventListener("input", (e) => {
+    let fieldset = e.target.parentElement.parentElement;
+    let box = e.target.parentElement;
+    let value = e.target.value;
+    let feedback = fieldset.querySelector(".create_msg-error-front.alcohol");
+    let msg = null;
+  
+    if (!validator.isLength(value, { min: 4 })) {
+      msg = "Este campo requiere un mínimo de 4 caracteres";
+    }
+  
+    if (msg) {
+      box.classList.remove("valid");
+      box.classList.add("invalid");
+      feedback.innerText = msg;
+    } else {
+      box.classList.remove("invalid");
+      box.classList.add("valid");
+      feedback.innerText = "";
+    }
+  });
+  
+  inputs.acidez.addEventListener("input", (e) => {
+    let fieldset = e.target.parentElement.parentElement;
+    let box = e.target.parentElement;
+    let value = e.target.value;
+    let feedback = fieldset.querySelector(".create_msg-error-front.acidez");
+    let msg = null;
+  
+    if (!validator.isLength(value, { min: 4 })) {
+      msg = "Este campo requiere un mínimo de 4 caracteres";
+    }
+  
+    if (msg) {
+      box.classList.remove("valid");
+      box.classList.add("invalid");
+      feedback.innerText = msg;
+    } else {
+      box.classList.remove("invalid");
+      box.classList.add("valid");
+      feedback.innerText = "";
+    }
+  });
+  
+  inputs.azucar.addEventListener("input", (e) => {
+    let fieldset = e.target.parentElement.parentElement;
+    let box = e.target.parentElement;
+    let value = e.target.value;
+    let feedback = fieldset.querySelector(".create_msg-error-front.azucar");
+    let msg = null;
+  
+    if (!validator.isLength(value, { min: 4 })) {
+      msg = "Este campo requiere un mínimo de 4 caracteres";
+    }
+  
+    if (msg) {
+      box.classList.remove("valid");
+      box.classList.add("invalid");
+      feedback.innerText = msg;
+    } else {
+      box.classList.remove("invalid");
+      box.classList.add("valid");
+      feedback.innerText = "";
+    }
+  });
+  
+  inputs.vista.addEventListener("input", (e) => {
+    let fieldset = e.target.parentElement.parentElement;
+    let box = e.target.parentElement;
+    let value = e.target.value;
+    let feedback = fieldset.querySelector(".create_msg-error-front.vista");
+    let msg = null;
+  
+    if (!validator.isLength(value, { min: 4 })) {
+      msg = "Este campo requiere un mínimo de 4 caracteres";
+    }
+  
+    if (msg) {
+      box.classList.remove("valid");
+      box.classList.add("invalid");
+      feedback.innerText = msg;
+    } else {
+      box.classList.remove("invalid");
+      box.classList.add("valid");
+      feedback.innerText = "";
+    }
+  });
+  
+  inputs.nariz.addEventListener("input", (e) => {
+    let fieldset = e.target.parentElement.parentElement;
+    let box = e.target.parentElement;
+    let value = e.target.value;
+    let feedback = fieldset.querySelector(".create_msg-error-front.nariz");
+    let msg = null;
+  
+    if (!validator.isLength(value, { min: 4 })) {
+      msg = "Este campo requiere un mínimo de 4 caracteres";
+    }
+  
+    if (msg) {
+      box.classList.remove("valid");
+      box.classList.add("invalid");
+      feedback.innerText = msg;
+    } else {
+      box.classList.remove("invalid");
+      box.classList.add("valid");
+      feedback.innerText = "";
+    }
+  });
+  
+  inputs.boca.addEventListener("input", (e) => {
+    let fieldset = e.target.parentElement.parentElement;
+    let box = e.target.parentElement;
+    let value = e.target.value;
+    let feedback = fieldset.querySelector(".create_msg-error-front.boca");
+    let msg = null;
+  
+    if (!validator.isLength(value, { min: 4 })) {
+      msg = "Este campo requiere un mínimo de 4 caracteres";
+    }
+  
+    if (msg) {
+      box.classList.remove("valid");
+      box.classList.add("invalid");
+      feedback.innerText = msg;
+    } else {
+      box.classList.remove("invalid");
+      box.classList.add("valid");
+      feedback.innerText = "";
+    }
+  });
+
   inputs.otros.addEventListener("input", (e) => {
     let fieldset = e.target.parentElement.parentElement;
     let box = e.target.parentElement;
--- public/scripts/editProduct.js
+++ public/scripts/editProduct.js
@@ -43,71 +43,225 @@ inputs.type.addEventListener("input", (e) => {
     box.classList.add("valid");
     feedback.innerText = "";
   }
+});
 
-  inputs.otros.addEventListener("input", (e) => {
-    let fieldset = e.target.parentElement.parentElement;
-    let box = e.target.parentElement;
-    let value = e.target.value;
-    let feedback = fieldset.querySelector(".edit_msg-error-front.otros");
-    let msg = null;
+inputs.price.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.price");
+  let msg = null;
 
-    if (!validator.isLength(value, { min: 20 })) {
-      msg = "La descripcion debe tener como mínimo de 20 caracteres";
+  if (!validator.isLength(value, { min: 3 })) {
+    msg = "El precio no puede ser menor a 3 cifras.";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.alcohol.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.alcohol");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 4 })) {
+    msg = "Este campo requiere un mínimo de 4 caracteres";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.acidez.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.acidez");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 4 })) {
+    msg = "Este campo requiere un mínimo de 4 caracteres";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.azucar.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.azucar");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 4 })) {
+    msg = "Este campo requiere un mínimo de 4 caracteres";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.vista.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.vista");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 4 })) {
+    msg = "Este campo requiere un mínimo de 4 caracteres";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.nariz.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.nariz");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 4 })) {
+    msg = "Este campo requiere un mínimo de 4 caracteres";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.boca.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.boca");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 4 })) {
+    msg = "Este campo requiere un mínimo de 4 caracteres";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.otros.addEventListener("input", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let box = e.target.parentElement;
+  let value = e.target.value;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.otros");
+  let msg = null;
+
+  if (!validator.isLength(value, { min: 20 })) {
+    msg = "La descripcion debe tener como mínimo de 20 caracteres";
+  }
+
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
+
+inputs.image.addEventListener("change", (e) => {
+  let fieldset = e.target.parentElement.parentElement;
+  let files = e.target.files;
+  let box = e.target.parentElement;
+  let feedback = fieldset.querySelector(".edit_msg-error-front.image");
+  let msg = null;
+
+  if (!files.length == 0) {
+    if (!validator.isMimeType(files[0].type)) {
+      msg = "La imagen no tiene un formato valido";
+    } else if (
+      !["jpg", "jpeg", "png", "gif"].includes(files[0].type.split("/")[1])
+    ) {
+      msg = "La imagen no tiene una extension valida";
     }
+  }
 
-    if (msg) {
-      box.classList.remove("valid");
-      box.classList.add("invalid");
-      feedback.innerText = msg;
-    } else {
-      box.classList.remove("invalid");
-      box.classList.add("valid");
-      feedback.innerText = "";
-    }
-  });
+  if (msg) {
+    box.classList.remove("valid");
+    box.classList.add("invalid");
+    feedback.innerText = msg;
+  } else {
+    box.classList.remove("invalid");
+    box.classList.add("valid");
+    feedback.innerText = "";
+  }
+});
 
-  inputs.image.addEventListener("change", (e) => {
-    let fieldset = e.target.parentElement.parentElement;
-    let files = e.target.files;
-    let box = e.target.parentElement;
-    let feedback = fieldset.querySelector(".edit_msg-error-front.image");
-    let msg = null;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let isCorrect = false;
 
-    if (!files.length == 0) {
-      if (!validator.isMimeType(files[0].type)) {
-        msg = "La imagen no tiene un formato valido";
-      } else if (
-        !["jpg", "jpeg", "png", "gif"].includes(files[0].type.split("/")[1])
-      ) {
-        msg = "La imagen no tiene una extension valida";
-      }
-    }
+  if (e.target.querySelectorAll(".edit_linea-form.invalid").length == 0) {
+    isCorrect = true;
+  }
 
-    if (msg) {
-      box.classList.remove("valid");
-      box.classList.add("invalid");
-      feedback.innerText = msg;
-    } else {
-      box.classList.remove("invalid");
-      box.classList.add("valid");
-      feedback.innerText = "";
-    }
-  });
-
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    let isCorrect = false;
-
-    if (e.target.querySelectorAll(".edit_linea-form.invalid").length == 0) {
-      isCorrect = true;
-    }
-
-    if (isCorrect) {
-      e.target.submit();
-    } else {
-      window.alert(
-        "Algunos datos no son correctos, por favor verificar los errores en los campos"
-      );
-    }
-  });
+  if (isCorrect) {
+    e.target.submit();
+  } else {
+    window.alert(
+      "Algunos datos no son correctos, por favor verificar los errores en los campos"
+    );
+  }
 });
