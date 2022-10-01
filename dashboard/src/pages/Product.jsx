cc32c20226f7edaee63f044166e71ce9ee7e53cb
AgustinCollados <collados_agustin@hotmail.com>
2022-10-01 16:14:46 -0300

    Componente product

--- dashboard/src/pages/Product.jsx
+++ dashboard/src/pages/Product.jsx
@@ -2,17 +2,16 @@ import { useEffect, useState } from "react";
 
 export default function Product() {
 
-  const dataProduct = 'http://localhost:3000/api/products/'
-  const [todos, setTodos] = useState()
+  const dataProduct = 'http://localhost:3000/api/products/1'
+  //const [todos, setTodos] = useState()
   const fetchApi = async () => {
-    const response = await fetch(dataProduct, {
-      mode: "no-cors"
-    }); 
-    
-    console.log(response.statusText)
+    const response = await fetch(dataProduct); 
+      console.log(response.statusText)
+
+    //const respondeJSON = response.json()
+
   }
 
-  
   useEffect(() => {
     fetchApi()
   }, [])
