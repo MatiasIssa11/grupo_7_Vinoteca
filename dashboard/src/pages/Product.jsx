c86891bb954e53cd3ec67505e9dd3f648936d3a5
alonsomatias88 <alonso.matias@live.com.ar>
2022-10-02 11:38:04 -0300

    Mati: corrección en el productsApy y componentes de detalles dinamicos

--- dashboard/src/pages/Product.jsx
+++ dashboard/src/pages/Product.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Product() {
-
-  const baseURL = 'http://localhost:3000/api/products'
-  const id = 3; // Falta logica del producto que se requiere
+  const baseURL = "http://localhost:3000/api/products";
+  const { id } = useParams();
 
   const [productDetail, setProductDetail] = useState("Cargando...");
- 
+
   const fetchApi = async () => {
     const endpoint = `${baseURL}/${id}`;
-    const response = await fetch(endpoint); 
-    const responseJSON = await response.json()
+    const response = await fetch(endpoint);
+    const responseJSON = await response.json();
     return responseJSON;
-  }
+  };
 
   useEffect(() => {
     fetchApi(id).then((responseJSON) => setProductDetail(responseJSON));
@@ -21,20 +21,25 @@ export default function Product() {
   return (
     <>
       <h3>PRODUCT {productDetail.id}</h3>
-        <h4>Marca: {productDetail.brand}</h4>
-        <h4>Typo: {productDetail.type}</h4>
-        <h4>Precio: {productDetail.price}</h4>
-        <h4>Precio de descuento: {productDetail.discuountPrice}</h4>
-
+      <h4>Marca: {productDetail.brand}</h4>
+      <h4>Typo: {productDetail.type}</h4>
+      <h4>Precio: {productDetail.price}</h4>
+      {productDetail.discountPrice ? (
+        <h4>Precio de descuento: {productDetail.discountPrice}</h4>
+      ) : (
+        <h4>Precio de descuento: No tiene</h4>
+      )}
+      <picture>
+        <img src={productDetail.image} alt={productDetail.type} />
+      </picture>
       <h3>DATOS ANALITICOS:</h3>
-        <h4>Alcohol: {productDetail.alcohol}</h4>
-        <h4>Acidez total: {productDetail.acidez}</h4>
-        <h4>Azucar total: {productDetail.azucar}</h4>
-
+      <h4>Alcohol: {productDetail.alcohol}</h4>
+      <h4>Acidez total: {productDetail.acidez}</h4>
+      <h4>Azucar total: {productDetail.azucar}</h4>
       <h3>CARACTERISTICAS DEGUSTATIVAS:</h3>
-        <h4>Vista: {productDetail.vista}</h4>
-        <h4>Nariz: {productDetail.nariz}</h4>
-        <h4>Boca: {productDetail.boca}</h4>
-      </>
+      <h4>Vista: {productDetail.vista}</h4>
+      <h4>Nariz: {productDetail.nariz}</h4>
+      <h4>Boca: {productDetail.boca}</h4>
+    </>
   );
 }
--- dashboard/src/pages/User.jsx
+++ dashboard/src/pages/User.jsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function User() {
-
-  const baseURL = 'http://localhost:3000/api/users'
-  const id = 3; // Falta logica del producto que se requiere
+  const baseURL = "http://localhost:3000/api/users";
+  const { id } = useParams();
 
   const [userDetail, setUserDetail] = useState("Cargando...");
- 
+
   const fetchApi = async () => {
     const endpoint = `${baseURL}/${id}`;
-    const response = await fetch(endpoint); 
-    const responseJSON = await response.json()
+    const response = await fetch(endpoint);
+    const responseJSON = await response.json();
     return responseJSON;
-  }
+  };
 
   useEffect(() => {
     fetchApi(id).then((responseJSON) => setUserDetail(responseJSON));
@@ -21,12 +21,13 @@ export default function User() {
   return (
     <>
       <h3>USUARIO {userDetail.id}</h3>
-        <h4>Nombre: {userDetail.nombre}</h4>
-        <h4>Apellido: {userDetail.apellido}</h4>
-        <h4>Email: {userDetail.email}</h4>
-        <h4>Fecha de nacimiento: {userDetail.fechaNacimiento}</h4>
-        <h4>Avatar: {userDetail.avatar}</h4>
-        
-      </>
+      <h4>Nombre: {userDetail.nombre}</h4>
+      <h4>Apellido: {userDetail.apellido}</h4>
+      <h4>Email: {userDetail.email}</h4>
+      <h4>Fecha de nacimiento: {userDetail.fechaNacimiento}</h4>
+      <picture>
+        <img src={userDetail.avatar} alt={userDetail.email} />
+      </picture>
+    </>
   );
 }
